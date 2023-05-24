944f064b675338b74a121b677b38f19cf4c340e7
BioDani <dtejada@unal.edu.co>
2023-05-24 18:33:03 -0500

    Wallet: get all and getByID
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -1,10 +1,13 @@
-const { Router } = require("express");
-const route = new Router();
+const express = require("express");
+const { Wallet } = require("../controllers");
+
+const router = express.Router()
+
+const BASE = "/api/v1/wallets";
+
+router.get( BASE , Wallet.getAllWallets);
+
+router.get(`${BASE}/:id`, Wallet.getWalletById);
 
 
-
-const BASE = "/api/v1/wallet";
-
-
-
-module.exports = route;
+module.exports = router;
