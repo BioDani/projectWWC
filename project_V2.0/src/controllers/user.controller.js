91890b8fa865f37d58898e299f073453449605a0
BioDani <dtejadah@unal.edu.co>
2023-06-15 15:58:42 -0500

    validación de email existente

--- project_V2.0/src/controllers/user.controller.js
+++ project_V2.0/src/controllers/user.controller.js
@@ -1,14 +1,15 @@
-const  { User }  = require("../models");
+const { User } = require('../models');
 
 exports.getAllUsers = async (_, res) => {
-  try {
-      const users = await User.find();
-      res.status(200).json(users);
-  } catch (error) {
-      console.log(error);
-  }
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
+/* 
 exports.addUser = async (req, res, next) => {
   try {
       const new_user = await User.create(req.body);
@@ -21,4 +22,27 @@ exports.addUser = async (req, res, next) => {
       console.error(error.message)
       next(error);
   }
-}
+} */
+
+exports.addUser = async (req, res, next) => {
+    try {
+        const { _id, role, name, surname, email, password } = await req.body;
+
+        const existingUserbyEmail = await User.findOne({ email });
+        
+        if (existingUserbyEmail) {
+            res.status(409).send({
+                status: 409,
+                message: 'Email is already registered',
+            });
+        }
+
+        const new_user = await User.create(req.body);
+        res.status(200).json({
+            status: 200,
+            message: "New user was created.",
+            user: new_user
+        })
+
+    } catch (error) {}
+};
