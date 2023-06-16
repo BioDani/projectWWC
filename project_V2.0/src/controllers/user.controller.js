36f796cd415de3bacb79a75ee61a7421dc65556f
BioDani <dtejada@unal.edu.co>
2023-06-16 01:40:31 -0500

    datos para poblar collections
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- project_V2.0/src/controllers/user.controller.js
+++ project_V2.0/src/controllers/user.controller.js
@@ -29,10 +29,10 @@ exports.addUser = async (req, res, next) => {
 
 exports.addUser = async (req, res, next) => {
     try {
-        const newUser = await req.body;
+        //const newUser = await req.body;
         const { _id, role, name, surname, email, password } = await req.body;
 
-        const existingUserbyEmail = await User.findOne( newUser[email] );
+        const existingUserbyEmail = await User.findOne( {email});
 
         if (existingUserbyEmail) {
             res.status(409).send({
@@ -44,21 +44,28 @@ exports.addUser = async (req, res, next) => {
 
         
         const hash = bcrypt.hashSync( password , saltRounds);
-        console.log(hash);
-        console.log(bcrypt.compareSync( password, hash));
-        newUser[password] = hash
+        //console.log(hash);
+        //console.log(bcrypt.compareSync( password, hash));
 
-        res.status(200).json({
+        const user = {
+            "_id": _id,
+            "role": role,
+            "name": name,
+            "surname": surname,
+            "email": email,
+            "password": hash
+        }
+
+/*         res.status(200).json({
             status: 200, 
-            hash: hash,
-            password: password
-        });
+            newUser: newUser
+        }); */
 
-        /*         const new_user = await User.create(req.body);
+        const new_user = await User.create(user);
         res.status(200).json({
             status: 200,
             message: "New user was created.",
             user: new_user
-        }) */
+        })
     } catch (error) {}
 };
