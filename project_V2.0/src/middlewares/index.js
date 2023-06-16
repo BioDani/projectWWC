8fc423e2ddf15fc19e6946dc782fe5b0f60f78d7
BioDani <dtejada@unal.edu.co>
2023-06-16 13:48:39 -0500

    index middlewares
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- project_V2.0/src/middlewares/index.js
+++ project_V2.0/src/middlewares/index.js
@@ -3,6 +3,7 @@ exports.validatorHandler = require("./validatorHandler")
 exports.addUserValidate = require("./user.joi.validation")
 exports.addAccountValidate = require("./account.joi.validation")
 exports.verifyJWT = require("./verifyJWT.middleware")
+exports.verifyRole  = require("./verifyRole.middleware")
 
 
 
--- project_V2.0/src/routes/user.route.js
+++ project_V2.0/src/routes/user.route.js
@@ -3,8 +3,8 @@ const { User , Auth} = require("../controllers");
 
 const { addUserValidate } = require("../middlewares/user.joi.validation")
 const  validatorHandler  = require("../middlewares/validatorHandler")
-const verifyJWT = require("../middlewares/verifyJWT.middleware")
-const verifyRole = require("../middlewares/verifyRole.middleware")
+
+const {verifyJWT , verifyRole } = require("../middlewares")
 
 const router = express.Router()
 const API_V1 = "/api/v1/";
@@ -14,7 +14,6 @@ router.post(API_V1+'register', validatorHandler( addUserValidate, 'body') , User
 router.post(API_V1+'login', Auth.login)
 
 router.get(API_V1+'users' , verifyJWT.isAuth, verifyRole.isAdmin ,User.getAllUsers)
-
 
 
 module.exports = router;
